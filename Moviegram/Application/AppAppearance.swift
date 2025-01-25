77867c4711dcc192e9b636ca7cd6c34ec2ebc7ce
been <leebin0603@naver.com>
2025-01-25 11:31:15 +0900

    Feat: 네비게이션바 appearance 설정 모든 네비게이션바 디자인 적용

---
+++ Moviegram/Application/AppAppearance.swift
@@ -0,0 +1,21 @@
+//
+//  AppAppearance.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/24/25.
+//
+
+import UIKit
+
+final class AppAppearance {
+    
+    static func configureAppearance() {
+        
+        // MARK: - 네비게이션바
+        UINavigationBar.appearance().tintColor = .point
+        UINavigationBar.appearance().topItem?.backButtonTitle = ""
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+    }
+    
+}
