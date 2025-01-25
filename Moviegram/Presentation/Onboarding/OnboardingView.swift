728302d0baf43313f90856e8ff52c7e2d639f958
been <leebin0603@naver.com>
2025-01-25 11:32:27 +0900

    Feat: 온보딩 화면 구현

---
+++ Moviegram/Presentation/Onboarding/OnboardingView.swift
@@ -0,0 +1,75 @@
+//
+//  OnboardingView.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/24/25.
+//
+
+import UIKit
+import SnapKit
+
+final class OnboardingView: BaseView {
+    
+    private let mainImageView = UIImageView()
+    private let mainLabel = UILabel()
+    private let subLabel = UILabel()
+    let startButton = UIButton()
+    
+    override func configureHierarchy() {
+        addSubViews(
+            mainImageView,
+            mainLabel,
+            subLabel,
+            startButton
+        )
+    }
+    
+    override func configureLayout() {
+        mainImageView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(30)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(500)
+        }
+        
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(mainImageView.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+        
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(mainLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(subLabel.snp.bottom).offset(30)
+            make.horizontalEdges.equalToSuperview().inset(15)
+            make.height.equalTo(44)
+        }
+    }
+    
+    override func configureView() {
+        mainImageView.image = .onboarding
+        mainImageView.contentMode = .scaleAspectFill
+        
+        mainLabel.text = "Onboarding"
+        mainLabel.textColor = .white
+        mainLabel.font = .Font.large.of(weight: .heavy)
+        mainLabel.textAlignment = .center
+        
+        subLabel.text = "당신만의 영화 세상,\nMoviegram을 시작해 보세요."
+        subLabel.textColor = .white
+        subLabel.font = .Font.medium.of(weight: .medium)
+        subLabel.textAlignment = .center
+        subLabel.numberOfLines = 0
+        
+        startButton.setTitle("시작하기", for: .normal)
+        startButton.titleLabel?.font = .Font.large.of(weight: .heavy)
+        startButton.setTitleColor(.point, for: .normal)
+        startButton.backgroundColor = .black
+        startButton.layer.cornerRadius = 20
+        startButton.layer.borderWidth = 2
+        startButton.layer.borderColor = UIColor.point.cgColor
+    }
+    
+}
---
+++ Moviegram/Presentation/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingViewController.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/24/25.
+//
+
+import UIKit
+
+final class OnboardingViewController: BaseViewController<OnboardingView> {
+    
+    override func configureView() {
+        mainView.startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func startButtonTapped() {
+        print(#function)
+        let vc = ProfileSettingViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
