4fc7a085ede1a602d7c74349e84f1987c75205f6
been <leebin0603@naver.com>
2025-01-25 14:31:47 +0900

    Feat: 프로필 설정 화면 구현

---
+++ Moviegram/Presentation/Profile/ProfileSettingView.swift
@@ -0,0 +1,121 @@
+//
+//  ProfileSettingView.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/24/25.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileSettingView: BaseView {
+    
+    let profileImageView = UIImageView()
+    let cameraView = UIView()
+    let cameraImageView = UIImageView()
+    
+    let nicknameTextField = UITextField()
+    let nicknameTextFieldUnderLine = UIView()
+    
+    let nicknameConditionStatusLabel = UILabel()
+    
+    let completionButton = UIButton()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+        
+        cameraView.layer.cornerRadius = cameraView.frame.width / 2
+    }
+
+    override func configureHierarchy() {
+        
+        cameraView.addSubview(cameraImageView)
+        
+        addSubViews(
+            profileImageView,
+            cameraView,
+            nicknameTextField,
+            nicknameTextFieldUnderLine,
+            nicknameConditionStatusLabel,
+            completionButton
+        )
+    }
+    
+    override func configureLayout() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(30)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(100)
+        }
+        
+        cameraView.snp.makeConstraints { make in
+            make.trailing.equalTo(profileImageView.snp.trailing)
+            make.bottom.equalTo(profileImageView.snp.bottom)
+            make.size.equalTo(30)
+        }
+        
+        cameraImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(20)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+        
+        nicknameTextFieldUnderLine.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(10)
+            make.height.equalTo(1)
+        }
+        
+        nicknameConditionStatusLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextFieldUnderLine.snp.bottom).offset(15)
+            make.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        completionButton.snp.makeConstraints { make in
+            make.top.equalTo(nicknameConditionStatusLabel.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(15)
+            make.height.equalTo(44)
+        }
+        
+    }
+    
+    override func configureView() {
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.image = UIImage(named: "profile_\(Int.random(in: 0...11))")
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.borderColor = UIColor.point.cgColor
+        profileImageView.layer.borderWidth = 3
+        
+        cameraView.backgroundColor = .point
+        cameraView.isUserInteractionEnabled = true
+        
+        cameraImageView.image = UIImage(systemName: "camera.fill")
+        cameraImageView.tintColor = .white
+        cameraImageView.contentMode = .scaleAspectFit
+        
+        nicknameTextField.textColor = .white
+        
+        nicknameTextFieldUnderLine.backgroundColor = .white
+        
+        nicknameConditionStatusLabel.textAlignment = .left
+        nicknameConditionStatusLabel.textColor = .point
+        nicknameConditionStatusLabel.font = .Font.small.of(weight: .medium)
+        
+        completionButton.setTitle("완료", for: .normal)
+        completionButton.titleLabel?.font = .Font.large.of(weight: .heavy)
+        completionButton.setTitleColor(.point, for: .normal)
+        completionButton.backgroundColor = .black
+        completionButton.layer.cornerRadius = 20
+        completionButton.layer.borderWidth = 2
+        completionButton.layer.borderColor = UIColor.point.cgColor
+        completionButton.isEnabled = false
+    }
+
+}
---
+++ Moviegram/Presentation/Profile/ProfileSettingViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ProfileSettingViewController.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/24/25.
+//
+
+import UIKit
+
+enum ConditionStatus {
+    case approve
+    case lengthLimit
+    case specialCharacterLimit
+    case numberLimit
+    
+    var description: String {
+        switch self {
+        case .approve:
+            return "사용할 수 있는 닉네임이에요"
+        case .lengthLimit:
+            return "2글자 이상 10글자 미만으로 설정해 주세요"
+        case .specialCharacterLimit:
+            return "닉네임에 @, #, $, % 는 포함할 수 없어요"
+        case .numberLimit:
+            return "닉네임에 숫자는 포함할 수 없어요"
+        }
+    }
+}
+
+final class ProfileSettingViewController: BaseViewController<ProfileSettingView> {
+    
+    let forbiddenStrings: [Character] = ["@", "#", "$", "%"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        navigationController?.navigationBar.topItem?.backButtonTitle = ""
+        title = "프로필 설정"
+    }
+    
+    override func configureView() {
+        mainView.nicknameTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
+        mainView.completionButton.addTarget(self, action: #selector(completionButtonTapped), for: .touchUpInside)
+        
+        mainView.profileImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(profileImageTapped)))
+        mainView.cameraView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(profileImageTapped)))
+        
+    }
+    
+    @objc func profileImageTapped() {
+        print(#function)
+        // 프로필 이미지 설정 화면으로 전환
+        let vc = ProfileImageSettingViewViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text else {
+            print("검사하지 않음")
+            return
+        }
+
+        for str in text {
+            // 특수문자 검사
+            if forbiddenStrings.contains(String(str)) {
+                updateNicknameConditionStatusLabel(ConditionStatus.specialCharacterLimit.description)
+                return
+            }
+            
+            // 숫자 검사
+            if let _ = Int(String(str)) {
+                updateNicknameConditionStatusLabel(ConditionStatus.numberLimit.description)
+                return
+            }
+        }
+        
+        // 특수문자, 숫자 검사 통과하면
+        // 길이 검사
+        if text.count >= 2 && text.count < 10 {
+            updateNicknameConditionStatusLabel(ConditionStatus.approve.description)
+        } else {
+            updateNicknameConditionStatusLabel(ConditionStatus.lengthLimit.description)
+        }
+        
+    }
+    
+    func updateNicknameConditionStatusLabel(_ text: String) {
+        mainView.nicknameConditionStatusLabel.text = text
+        
+        mainView.completionButton.isEnabled = false // approve인 경우에만, true로 설정
+        if text == ConditionStatus.approve.description {
+            // 완료 버튼 누를 수 있도록
+            mainView.completionButton.isEnabled = true
+        }
+    }
+    
+    @objc func completionButtonTapped() {
+        //WindowRootViewController 가 [메인 화면]으로 교체
+        // 여기서 유저 정보 저장해야될 듯?
+        print(#function)
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first else { return }
+        
+        window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window.makeKeyAndVisible()
+    }
+    
+}
