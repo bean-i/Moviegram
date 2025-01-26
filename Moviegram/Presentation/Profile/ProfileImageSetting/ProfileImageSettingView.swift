3670f53ff6b947ac45f0b6edd2becfbfe54d0cdf
been <leebin0603@naver.com>
2025-01-26 15:11:57 +0900

    Feat: 프로필이미지세팅 화면 구현

---
+++ Moviegram/Presentation/Profile/ProfileImageSetting/ProfileImageSettingView.swift
@@ -0,0 +1,85 @@
+//
+//  ProfileImageSettingView.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/25/25.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileImageSettingView: BaseView {
+
+    let profileImageView = ProfileImageCustomView()
+    let cameraView = UIView()
+    let cameraImageView = UIImageView()
+    
+    var profileImageCollectionView = UICollectionView(frame: .zero, collectionViewLayout: profileImageCollectionViewLayout())
+    
+    static func profileImageCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let inset: CGFloat = 30
+        let spacing: CGFloat = 10
+        let count: CGFloat = 4
+        let deviceWidth = UIScreen.main.bounds.width
+        let size = (deviceWidth - (2 * inset) - ((count - 1) * spacing)) / count
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+        layout.minimumInteritemSpacing = spacing
+        layout.minimumLineSpacing = spacing
+        layout.itemSize = CGSize(width: size, height: size)
+        return layout
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        cameraView.layer.cornerRadius = cameraView.frame.width / 2
+    }
+    
+    override func configureHierarchy() {
+        cameraView.addSubview(cameraImageView)
+        
+        addSubViews(
+            profileImageView,
+            cameraView,
+            profileImageCollectionView
+        )
+    }
+    
+    override func configureLayout() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(30)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(100)
+        }
+        
+        cameraView.snp.makeConstraints { make in
+            make.trailing.equalTo(profileImageView.snp.trailing)
+            make.bottom.equalTo(profileImageView.snp.bottom)
+            make.size.equalTo(30)
+        }
+        
+        cameraImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(20)
+        }
+        
+        profileImageCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(50)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        cameraView.backgroundColor = .point
+        cameraView.isUserInteractionEnabled = true
+        
+        cameraImageView.image = UIImage(systemName: "camera.fill")
+        cameraImageView.tintColor = .white
+        cameraImageView.contentMode = .scaleAspectFit
+        
+        profileImageCollectionView.backgroundColor = .clear
+    }
+    
+}
---
+++ Moviegram/Presentation/Profile/ProfileImageSetting/ProfileImageSettingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileImageSettingViewController.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/25/25.
+//
+
+import UIKit
+
+final class ProfileImageSettingViewController: BaseViewController<ProfileImageSettingView> {
+    
+    var selectedImageNumber: Int = 0
+    
+    override func configureView() {
+        view.backgroundColor = .black
+        navigationController?.navigationBar.topItem?.backButtonTitle = ""
+        title = "프로필 이미지 설정"
+        
+        mainView.profileImageView.imageNumber = selectedImageNumber
+        mainView.profileImageView.isSelected = true
+        
+        mainView.profileImageCollectionView.delegate = self
+        mainView.profileImageCollectionView.dataSource = self
+        mainView.profileImageCollectionView.register(ProfileImageCollectionViewCell.self, forCellWithReuseIdentifier: ProfileImageCollectionViewCell.identifier)
+    }
+    
+}
+
+extension ProfileImageSettingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileImageCollectionViewCell.identifier, for: indexPath) as! ProfileImageCollectionViewCell
+        
+        cell.isSelected = false
+        cell.imageNumber = indexPath.item
+        
+        if indexPath.item == selectedImageNumber {
+            cell.isSelected = true
+            collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .init())
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let selectedCell = collectionView.cellForItem(at: indexPath) as? ProfileImageCollectionViewCell else {
+            print("셀 선택 오류")
+            return
+        }
+        
+        mainView.profileImageView.imageNumber = selectedCell.imageNumber
+    }
+
+}
