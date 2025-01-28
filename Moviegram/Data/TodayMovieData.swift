230d7ff7474690b3f5f9794452eec74024896d77
been <leebin0603@naver.com>
2025-01-28 12:59:50 +0900

    Chore: Movie데이터 파일 분리 적용

--- Moviegram/Data/TodayMovieData.swift
+++ Moviegram/Data/TodayMovieData.swift
@@ -11,25 +11,3 @@ struct TodayMovieData: Decodable {
     let page: Int
     let results: [Movie]
 }
-
-struct Movie: Decodable {
-    let id: Int
-    let backdropURL: String
-    let title: String
-    let overview: String
-    let posterURL: String
-    let genreID: [Int]
-    let releaseDate: String
-    let averageRating: Double
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case backdropURL = "backdrop_path"
-        case title
-        case overview
-        case posterURL = "poster_path"
-        case genreID = "genre_ids"
-        case releaseDate = "release_date"
-        case averageRating = "vote_average"
-    }
-}
