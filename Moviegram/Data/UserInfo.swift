bd513a4e4f312161846427de9f3a3a6113d01b43
been <leebin0603@naver.com>
2025-01-29 21:36:04 +0900

    Refactor: 사용자 정보 키 enum으로 관리

--- Moviegram/Data/UserInfo.swift
+++ Moviegram/Data/UserInfo.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
-class UserInfo {
+enum UserInfoKey: String, CaseIterable {
+    case nicknameKey = "nickname"
+    case imageNumberKey = "imageNumber"
+    case joinDateKey = "joinDate"
+    case storedMoviesKey = "storedMovies"
+}
+
+final class UserInfo {
     
     static let shared = UserInfo()
     
@@ -16,48 +23,43 @@ class UserInfo {
     // 좋아요 누른 영화 리스트 + UserDefaults에 저장할 배열
     static var storedMovieList: Set<Int> = Set(UserInfo.shared.storedMovies ?? [])
     
-    let nicknameKey = "nickname"
-    let imageNumberKey = "imageNumber"
-    let joinDateKey = "joinDate"
-    let storedMoviesKey = "storedMovies"
-    
     var nickname: String? {
         get {
-            UserDefaults.standard.string(forKey: nicknameKey)
+            UserDefaults.standard.string(forKey: UserInfoKey.nicknameKey.rawValue)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: nicknameKey)
+            UserDefaults.standard.set(newValue, forKey: UserInfoKey.nicknameKey.rawValue)
         }
     }
     
     var imageNumber: Int? {
         get {
-            UserDefaults.standard.integer(forKey: imageNumberKey)
+            UserDefaults.standard.integer(forKey: UserInfoKey.imageNumberKey.rawValue)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: imageNumberKey)
+            UserDefaults.standard.set(newValue, forKey: UserInfoKey.imageNumberKey.rawValue)
         }
     }
     
     var joinDate: String? {
         get {
-            UserDefaults.standard.string(forKey: joinDateKey)
+            UserDefaults.standard.string(forKey: UserInfoKey.joinDateKey.rawValue)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: joinDateKey)
+            UserDefaults.standard.set(newValue, forKey: UserInfoKey.joinDateKey.rawValue)
         }
     }
     
     var storedMovies: [Int]? {
         get {
-            UserDefaults.standard.object(forKey: storedMoviesKey) as? [Int]
+            UserDefaults.standard.object(forKey: UserInfoKey.storedMoviesKey.rawValue) as? [Int]
         }
         set {
             if let addMovies = newValue {
                 for movie in addMovies {
                     UserInfo.storedMovieList.insert(movie) // 집합에 넣어주기
                 }
-                UserDefaults.standard.set(Array(UserInfo.storedMovieList), forKey: storedMoviesKey)
+                UserDefaults.standard.set(Array(UserInfo.storedMovieList), forKey: UserInfoKey.storedMoviesKey.rawValue)
             }
         }
     }
