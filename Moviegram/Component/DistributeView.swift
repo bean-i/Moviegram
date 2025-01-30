1a0a5611630cfc2e4e4b9fefdb5cfcc801ba5969
been <leebin0603@naver.com>
2025-01-30 16:58:34 +0900

    Feat: 영화 상세 화면의 영화 정보 distribution 커스텀 뷰 구현

---
+++ Moviegram/Component/DistributeView.swift
@@ -0,0 +1,32 @@
+//
+//  DistributeView.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+
+final class DistributeView: BaseView {
+
+    let distributeLabel = UILabel()
+    
+    override func configureHierarchy() {
+        addSubview(distributeLabel)
+    }
+    
+    override func configureLayout() {
+        distributeLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.center.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        distributeLabel.text = "|"
+        distributeLabel.font = .Font.large.of(weight: .light)
+        distributeLabel.textColor = .customGray
+    }
+
+}
