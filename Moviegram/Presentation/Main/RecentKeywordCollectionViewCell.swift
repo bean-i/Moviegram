1b4f7da02b486c52c4b80d8c3bf9879ba565f7b7
been <leebin0603@naver.com>
2025-01-30 21:49:43 +0900

    Feat: 최근 검색어 키워드 컬렉션뷰셀 구현

---
+++ Moviegram/Presentation/Main/RecentKeywordCollectionViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  RecentKeywordCollectionViewCell.swift
+//  Moviegram
+//
+//  Created by 이빈 on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+
+final class RecentKeywordCollectionViewCell: BaseCollectionViewCell {
+    
+    static let identifier = "RecentKeywordCollectionViewCell"
+    
+    let keywordView = UIView()
+    let keywordLabel = UILabel()
+    let xButton = UIButton()
+    
+    override func configureHierarchy() {
+        keywordView.addSubViews(
+            keywordLabel,
+            xButton
+        )
+        
+        addSubview(keywordView)
+    }
+    
+    override func configureLayout() {
+        keywordView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        keywordLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(10)
+        }
+        
+        xButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(keywordLabel.snp.trailing).offset(10)
+            make.size.equalTo(10)
+        }
+    }
+    
+    override func configureView() {
+        keywordView.backgroundColor = .white
+        keywordView.layer.cornerRadius = 15
+        
+        keywordLabel.textColor = .black
+        keywordLabel.font = .Font.medium.of(weight: .medium)
+        
+        xButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        xButton.tintColor = .black
+    }
+    
+}
