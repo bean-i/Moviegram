3ff3b4b27e67c0c1790393d7d5625452e47cdae1
been <leebin0603@naver.com>
2025-01-30 16:57:41 +0900

    Refactor: 장르 enum 적용

--- Moviegram/Component/GenreBox.swift
+++ Moviegram/Component/GenreBox.swift
@@ -13,7 +13,7 @@ final class GenreBox: BaseView {
     let genreLabel = UILabel()
     var genreTitleId: Int = 0 {
         didSet {
-            genreLabel.text = configureGenreTitle(id: genreTitleId)
+            genreLabel.text = Genre.getGenre(id: genreTitleId)
         }
     }
     
@@ -33,33 +33,6 @@ final class GenreBox: BaseView {
         
         genreLabel.font = .Font.small.of(weight: .medium)
         genreLabel.textColor = .white
-    }
-    
-    func configureGenreTitle(id: Int) -> String {
-        
-        switch id {
-        case 28: return "액션"
-        case 16: return "애니메이션"
-        case 80: return "범죄"
-        case 18: return "드라마"
-        case 14: return "판타지"
-        case 27: return "공포"
-        case 9648: return "미스터리"
-        case 878: return "SF"
-        case 53: return "스릴러"
-        case 37: return "서부"
-        case 12: return "모험"
-        case 35: return "코미디"
-        case 99: return "다큐멘터리"
-        case 10751: return "가족"
-        case 36: return "역사"
-        case 10402: return "음악"
-        case 10749: return "로맨스"
-        case 10770: return "TV 영화"
-        case 10752: return "전쟁"
-        default: return "장르 없음"
-        }
-        
     }
     
 }
