f41d21cea3697d5749770839461dcd617fec69fc
been <leebin0603@naver.com>
2025-01-30 14:22:41 +0900

    Feat: 네비게이션바 스크롤 디자인 적용

--- Moviegram/Application/AppAppearance.swift
+++ Moviegram/Application/AppAppearance.swift
@@ -12,8 +12,13 @@ final class AppAppearance {
     static func configureAppearance() {
         
         // MARK: - 네비게이션바
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .black
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         UINavigationBar.appearance().tintColor = .point
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
         
     }
     
