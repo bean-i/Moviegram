7a4ce03db636749a588a9fcd9224fd1e48a4509f
been <leebin0603@naver.com>
2025-02-05 20:03:29 +0900

    Feat: String 확장 - 폰트에 따른 문자열 길이 구하기

--- Moviegram/Extension/String+.swift
+++ Moviegram/Extension/String+.swift
@@ -5,13 +5,20 @@
 //  Created by 이빈 on 1/28/25.
 //
 
-import Foundation
+import UIKit
 
 extension String {
     // 2014-12-18 문자열을
     // 2014. 12. 18로 만들어서 반환
     func convertDateString() -> String {
         return self.replacingOccurrences(of: "-", with: ". ")
+    }
+    
+    // 텍스트 길이 계산하기
+    func calculateTextWidth(font: UIFont) -> CGFloat {
+        let attributes = [NSAttributedString.Key.font: font]
+        let textSize = (self as NSString).size(withAttributes: attributes as [NSAttributedString.Key: Any])
+        return textSize.width
     }
     
 }
