be5d6469c784899cf443e702968db43fb4d93139
been <leebin0603@naver.com>
2025-02-08 15:43:41 +0900

    Feat: Observable 구현

---
+++ Moviegram/Presentation/Observable.swift
@@ -0,0 +1,33 @@
+//
+//  Observable.swift
+//  Moviegram
+//
+//  Created by 이빈 on 2/8/25.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+    
+    func lazyBind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+    
+}
